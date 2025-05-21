972e9454e8d3885482d4a621f67345d4a40d19ac
oreo1001 <ohsimon77@naver.com>
2025-05-21 19:18:59 +0900

    final docker 테스트를 위한 커밋

--- docker.sh
+++
@@ -1,4 +0,0 @@
-docker build -t stal-franchise-v1:1.0.0 -f Dockerfile .
-docker load -i stal-franchise.tar # 테스트 json 폴더 볼륨 마운트, API KEY 넣기 docker run --rm -it --gpus all -v your_test_folder:/app/test -e GEMINI_API_KEY=your_api_key stal-franchise:latest /bin/bash # 도커 내부에서 bash 파일 실행 bash franchise_RAG.sh /app/test
-
-docker run --rm -it --gpus all -v ./test:/app/test -e GEMINI_API_KEY=AIzaSyAgPaXsKKIzBJL4XrHDEm4OWdrj0o4M9G4 stal-franchise-v1:latest /bin/bash # 도커 내부에서 bash 파일 실행 bash franchise_RAG.sh /app/test
---
+++ sbert-wrapping.py
@@ -0,0 +1,18 @@
+from sentence_transformers import SentenceTransformer, models
+
+# 1. 기존 transformer 모델 로드
+word_embedding_model = models.Transformer('./stal-v2', max_seq_length=8192)
+
+# 2. Pooling 설정 (mean-pooling 추천)
+pooling_model = models.Pooling(
+    word_embedding_model.get_word_embedding_dimension(),
+    pooling_mode_mean_tokens=True,
+    pooling_mode_cls_token=False,
+    pooling_mode_max_tokens=False
+)
+
+# 3. 통합 SentenceTransformer 구성
+model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
+# 4. 새로운 디렉토리에 저장 (sbert 포맷으로)
+model.save('./squad-v1')  # huggingface에 올릴 때도 이걸 기준으로
